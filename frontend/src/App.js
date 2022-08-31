0dab08a245c1852790fdf103e9976d82833a896c
UnknownXC <86107437+UnknownXC@users.noreply.github.com>
2022-08-31 10:51:26 +0800

    login-token-authenticate
    //added login UI n Login.css
    //Modified App.js
    //npm install --save-dev express cors
    //added server.js
    //added useToken.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,10 +8,26 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import JobDetails from './pages/JobDetails'
 import { useState } from 'react'
 import jobs from './data/jobs'
+import useToken from './useToken'//steve added
+
+
+function setToken(userToken) {
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+}//steve added
+
+function getToken() {
+    const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}//steve added
 
 function App() {
+    const { token, setToken } = useToken();//steve added
     const [jobData, setJobData] = useState(jobs)
 
+    if(!token) {
+        return <Login setToken={setToken} />
+      }//steve added
     return (
         <Router>
             <Header />
