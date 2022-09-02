6aedc5031154afdfdb79115316763c8b48fc925f
UnknownXC <86107437+UnknownXC@users.noreply.github.com>
2022-09-03 04:37:49 +0800

    Added mongodb
    Left with pages + css

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,22 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
 const Footer = () => {
     return (
-        <footer id='footer'>
-            <div className='footer container'>
-                <h1>Copyright &copy; 2022 GrabJobs All Reserved</h1>
-            </div>
+        <footer id='footer'
+            style={{
+                width: "100%",
+                position: "inline",
+                bottom: 0,
+                display: "flex",
+                justifyContent: "center",
+            }}
+        >
+            <Container>
+                <Row>
+                    <div className="footer container">Copyright &copy; 2022 GrabJobs. All rights reserved.</div>
+                </Row>
+            </Container>
         </footer>
     )
 }
